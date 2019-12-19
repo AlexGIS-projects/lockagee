283e02222f815b7bf9672efa7519eb479be9c030
Alex Eberle <alex.eberle@uky.edu>
2019-12-18 21:51:58 -0500

    update potree

---
+++ potree/pointclouds/index/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 2446845,
+    "boundingBox": {
+        "lx": 5582320.51,
+        "ly": 3939595.83,
+        "lz": 891.3299999999999,
+        "ux": 5584320.5,
+        "uy": 3941595.8200000005,
+        "uz": 2891.3200000002234
+    },
+    "tightBoundingBox": {
+        "lx": 5582320.51,
+        "ly": 3939595.83,
+        "lz": 891.3299999999999,
+        "ux": 5584320.5,
+        "uy": 3941595.82,
+        "uz": 1423.81
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 17.32042121887207,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
